3b4af058428093e4f7fe90db8edaf6f4d10a9e87
Shurygina-Jul <melam.2916@gmail.com>
2021-03-25 23:21:16 +0300

    состояние внутри компоненты

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import './styles.css'
+
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
+//это модель
+let state = 0
+
+//рендерим модель в шаблон
+function render(){
+  counter.textContent=state.toString()
+}
+//если произошел клик, меняем модель напрямую и рендерим 
+addBtn.addEventListener('click', () =>{
+  state++
+  render()
+})
+
+subBtn.addEventListener('click', () =>{
+  state--
+render()
+})
+//через 2 секунды счетчик увеличивается на 1
+asyncBtn.addEventListener('click', () =>{
+  setTimeout(()=>{
+    state++
+    render()
+  },2000 )
+ 
+})
+//смена темы на dark
+themeBtn.addEventListener('click', () =>{
+  document.body.classList.toggle('dark')
+render()
+})
+render()
